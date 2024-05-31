eaf182c2eea1515ccf32827d2c0e80d58ab955ac
ikemarcus <ikemarcus@hotmail.nl>
2024-05-31 09:49:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,35 @@ function generatePassword() {
     const symbols = '!@#$%&*()/';
 
     let characters = lowercase + numbers;
-    if (includeUppercase) characters += uppercase;
-    if (includeSymbols) characters += symbols;
+    let mandatoryCharacters = '';
+
+    if (includeUppercase) {
+        characters += uppercase;
+        mandatoryCharacters += uppercase[Math.floor(Math.random() * uppercase.length)];
+    }
+
+    if (includeSymbols) {
+        characters += symbols;
+        mandatoryCharacters += symbols[Math.floor(Math.random() * symbols.length)];
+    }
 
     let password = '';
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < length - mandatoryCharacters.length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         password += characters[randomIndex];
     }
 
+    password += mandatoryCharacters;
+    password = shuffle(password);
+
     document.getElementById('password').textContent = password;
+}
+
+function shuffle(string) {
+    const array = string.split('');
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array.join('');
 }
